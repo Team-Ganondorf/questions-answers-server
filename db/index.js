fd0f2a1ce26bf9863b9223b16856cfd664157c11
Sarah Handley <handleysarah99@gmail.com>
2022-11-07 14:40:50 -0600

    finish get req for answers

--- db/index.js
+++ db/index.js
@@ -20,34 +20,53 @@ db.connectAsync()
 
 const getQuestions = (product_id, start, count) => {
   return db.queryAsync(
-    `SELECT q.question_id, q.question_body, q.question_date, q.asker_name, q.question_helpfulness, q.reported,
-    a.id, a.question_id, a.body, a.date, a.answerer_name, a.helpfulness, a.reported
-    FROM questions q, answers a
+    `SELECT q.question_id, q.question_body, TO_CHAR(TO_TIMESTAMP(q.question_date / 1000), 'YYYY-MM-DD"T"HH24:MI:SS.MSZ') as question_date,
+    q.asker_name, q.question_helpfulness,
+    CASE q.reported
+      WHEN 0 THEN false
+      ELSE true
+    END as reported,
+    CASE
+      WHEN count(a.id) = 0 THEN '{}'
+      ELSE (json_build_object(a.id, json_build_object(
+        'id', a.id, 'body', a.body, 'date', TO_CHAR(TO_TIMESTAMP(a.date / 1000), 'YYYY-MM-DD"T"HH24:MI:SS.MSZ'), 'answerer_name', a.answerer_name, 'helpfulness', a.helpfulness
+      )))
+    END as answers
+    FROM questions q LEFT JOIN answers a
+    ON q.question_id = a.question_id AND a.reported = 0
     WHERE q.reported = 0
+    GROUP BY q.question_id, a.id
     OFFSET ${start} ROWS
     FETCH NEXT ${count} ROWS ONLY;`
   )
   .then((data) => {
     console.log(data[0].rows);
   })
-  .then(() => {
-    return db.queryAsync(
-      `SELECT id, body, date, answerer_name, helpfulness
-      FROM answers
-      WHERE reported = 0 AND question_id IN
-      (SELECT question_id
-      FROM questions
-      WHERE reported = 0
-      OFFSET ${start} ROWS
-      FETCH NEXT ${count} ROWS ONLY)
-      ORDER BY question_id;`
-    )
+  .catch((err) => console.log(err))
+};
+
+const getAnswers = (question_id, start, count) => {
+  return db.queryAsync(
+    `SELECT a.id as answer_id, a.body, TO_CHAR(TO_TIMESTAMP(a.date / 1000), 'YYYY-MM-DD"T"HH24:MI:SS.MSZ') as date, a.answerer_name, a.helpfulness,
+    CASE
+      WHEN count(p.id) = 0 THEN '[]'
+      ELSE json_agg(json_build_object('id', p.id, 'url', p.url))
+    END as photos
+    FROM answers a LEFT JOIN photos p
+    ON a.id = p.answer_id
+    WHERE a.question_id = ${question_id} AND a.reported = 0
+    GROUP BY a.id, p.id
+    OFFSET ${start} ROWS
+    FETCH NEXT ${count} ROWS ONLY;`
+  )
+  .then((data) => {
+    return data[0].rows;
   })
-  .then((data) => console.log(data[0].rows))
   .catch((err) => console.log(err))
 };
 
 module.exports = {
   db,
-  getQuestions
+  getQuestions,
+  getAnswers
 };
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const db = require('../db/index.js').db;
 const getQuestions = require('../db/index.js').getQuestions;
+const getAnswers = require('../db/index.js').getAnswers;
 
 const app = express();
 
@@ -17,10 +18,52 @@ app.get('/qa/questions', (req, res) => {
   let count = req.query.count ? req.query.count : 5;
   let start = (page - 1) * count;
   getQuestions(product_id, start, count)
-    .then(() => console.log('success'))
-    .catch((err) => console.log('failure'))
+    .then(() => console.log('questions req made it back to server'))
+    .catch((err) => console.log('questions req failure'))
 });
 
+app.get('/qa/questions/:question_id/answers', (req, res) => {
+  let question_id = req.params.question_id;
+  let page = req.query.page ? req.query.page : 1;
+  let count = req.query.count ? req.query.count : 5;
+  let start = (page - 1) * count;
+  getAnswers(question_id, start, count)
+    .then((data) => res.send({
+      'question': question_id,
+      'page': page,
+      'count': count,
+      'results': data
+    }))
+    .catch((err) => {
+      console.log('answers req failure');
+      res.status(501);
+    })
+});
+
+app.post('qa/questions', (req, res) => {
+  console.log(req.query);
+  console.log('finish post question req');
+});
+
+app.post('/qa/questions/:question_id/answers', (req, res) => {
+  console.log('finish post answer req');
+});
+
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  console.log('finish put question helpful req');
+});
+
+app.put('/qa/questions/:question_id/report', (req, res) => {
+  console.log('finish put question report req');
+});
+
+app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+  console.log('finish put answer helpful req');
+});
+
+app.put('/qa/answers/:answer_id/report', (req, res) => {
+  console.log('finish put answer report req');
+});
 
 app.listen(process.env.PORT);
 console.log(`server is listening at ${process.env.HOST}:${process.env.PORT}`);
